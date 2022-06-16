60183f5dede2f97d618eb00cf4b3e2b26fe19ef2
mikhail-lyubimtsev <mlubimtsev@yandex.ru>
2022-06-16 15:20:22 +0300

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+# A/B калькулятор
+
+import tkinter as tk
+
+# Создание главного окна
+root = tk.Tk()
+root.geometry("280x300")
+root.title("A/B калькулятор")
+
+# Запуск цикла mainloop
+root.mainloop()
