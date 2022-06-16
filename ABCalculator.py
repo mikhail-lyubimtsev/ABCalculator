052fcd1030de49d466c5f499a5032ce8b0040575
mikhail-lyubimtsev <mlubimtsev@yandex.ru>
2022-06-16 18:38:04 +0300

    Добавление окна результатов

--- ABCalculator.py
+++ ABCalculator.py
@@ -6,6 +6,15 @@ import tkinter as tk
 def do_close():
     root.destroy()
 
+def popup_window():
+    window = tk.Toplevel()
+    window.geometry("280x300")
+    window.title("A/B результат")
+    
+    # Добавление кнопки закрытия окна
+    btnClosePopup = tk.Button(window, text = "Закрыть", font = ('Helvetica', 10, 'bold'), command = window.destroy)
+    btnClosePopup.place(x = 160, y = 250, width = 90, height = 30)
+    
 # Создание главного окна
 root = tk.Tk()
 root.geometry("280x300")
@@ -52,7 +61,7 @@ entConversions2.place(x = 115, y = 205, width = 90, height = 20)
 
 
 # Добавление кнопки "Рассчитать"
-btnProcess = tk.Button(root, text = "Рассчитать", font = ('Helvetica', 10, 'bold'))
+btnProcess = tk.Button(root, text = "Рассчитать", font = ('Helvetica', 10, 'bold'), command = popup_window)
 btnProcess.place(x = 25, y = 250, width = 90, height = 30)
 
 # Добавление кнопки закрытия программы
